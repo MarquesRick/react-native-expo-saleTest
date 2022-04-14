ce4397917791a2c061882374cccd43ff248767fc
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-13 21:26:31 -0300

    alteracao de porta do json server

--- src/servicos/apiLeiloes.js
+++ src/servicos/apiLeiloes.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 // create axios
 const instance = axios.create({
-  baseURL: 'http://192.168.1.3:3000/',
+  baseURL: "http://192.168.0.101:3000/",
   timeout: 1000,
 });
 
