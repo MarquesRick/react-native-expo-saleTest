6111acde5fbb5766028930750a5ebfea176ac97b
Henrique Marques <37425086+MarquesRick@users.noreply.github.com>
2022-04-18 21:29:14 -0300

    testando componentes e usuando waitFor

--- __tests__/telas/Leilao/EnviaLance.test.js
+++ __tests__/telas/Leilao/EnviaLance.test.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
 import EnviaLance from '../../../src/telas/Leilao/componentes/EnviaLance';
-import { ENVIADO } from '../../../src/negocio/constantes/estadosLance';
+import {
+  ENVIADO,
+  NAO_ENVIADO,
+} from '../../../src/negocio/constantes/estadosLance';
+
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 describe('telas/Leilao/componentes/EnviaLance', () => {
-  it('deve enviar o lance quando o botão for pressionado', () => {
+  it('deve enviar o lance quando o botão for pressionado', async () => {
     //fn: mokar função
     const enviaLance = jest.fn(() => new Promise(resolve => resolve(ENVIADO)));
-    const { toJSON } = render(
+    const { getByPlaceholderText, getByA11yHint, getByText } = render(
       <EnviaLance enviaLance={enviaLance} cor="blue" />,
     );
+
+    const input = getByPlaceholderText('R$');
+    const botao = getByA11yHint('Enviar lance');
+    //evento para alterar o valor de um texto (input q será alterado, valor alterado)
+    fireEvent.changeText(input, '10');
+    //evento para pressionar botão (botão a ser pressionado)
+    fireEvent.press(botao);
+    await waitFor(() => {
+      expect(enviaLance).toHaveBeenCalledWith('10');
+      expect(getByText(ENVIADO)).toBeTruthy();
+    });
+    //testar erros
+    expect(() => getByText(NAO_ENVIADO)).toThrow();
   });
 });
--- src/telas/Leilao/componentes/EnviaLance.js
+++ src/telas/Leilao/componentes/EnviaLance.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
-import { Text, TextInput, TouchableOpacity, StyleSheet, Platform } from 'react-native';
+import {
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  Platform,
+} from 'react-native';
 import { BlurView } from 'expo-blur';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { ENVIADO } from '../../../negocio/constantes/estadosLance';
@@ -27,75 +33,84 @@ export default function EnviaLance({ enviaLance, cor }) {
     }
 
     setEnviando(false);
-  }
+  };
 
-  return <BlurView intensity={Platform.OS === 'ios' ? 10 : 100} style={estilos.fundo} tint="light">
-    {!!erro && <Text style={estilos.erro}>{erro}</Text>}
-    {!!sucesso && <Text style={estilos.sucesso}>{sucesso}</Text>}
-    <TextInput 
-      value={valor} 
-      onChangeText={setValor}
-      placeholder="R$"
-      editable={!enviando}
-      style={estilos.entrada}
-      keyboardType="decimal-pad"
-    />
-    <TouchableOpacity 
-      onPress={validaEnvio}
-      disabled={enviando}
-      style={estilos.botao}>
-      <FontAwesome5 name="check" size={24} color="#14181B" />
-    </TouchableOpacity>
-  </BlurView>
+  return (
+    <BlurView
+      intensity={Platform.OS === 'ios' ? 10 : 100}
+      style={estilos.fundo}
+      tint="light"
+    >
+      {!!erro && <Text style={estilos.erro}>{erro}</Text>}
+      {!!sucesso && <Text style={estilos.sucesso}>{sucesso}</Text>}
+      <TextInput
+        value={valor}
+        onChangeText={setValor}
+        placeholder="R$"
+        editable={!enviando}
+        style={estilos.entrada}
+        keyboardType="decimal-pad"
+      />
+      <TouchableOpacity
+        accessibilityHint="Enviar lance"
+        onPress={validaEnvio}
+        disabled={enviando}
+        style={estilos.botao}
+      >
+        <FontAwesome5 name="check" size={24} color="#14181B" />
+      </TouchableOpacity>
+    </BlurView>
+  );
 }
 
-const funcaoEstilos = (cor, erro) => StyleSheet.create({
-  fundo: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
+const funcaoEstilos = (cor, erro) =>
+  StyleSheet.create({
+    fundo: {
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      right: 0,
 
-    flexDirection: 'row',
-    padding: 8,
-    flexWrap: 'wrap',
-  },
-  erro: {
-    width: '100%',
-    marginHorizontal: 8,
+      flexDirection: 'row',
+      padding: 8,
+      flexWrap: 'wrap',
+    },
+    erro: {
+      width: '100%',
+      marginHorizontal: 8,
 
-    color: '#FF0000',
+      color: '#FF0000',
 
-    fontSize: 14,
-    marginBottom: 8,
-  },
-  sucesso: {
-    width: '100%',
-    marginHorizontal: 8,
+      fontSize: 14,
+      marginBottom: 8,
+    },
+    sucesso: {
+      width: '100%',
+      marginHorizontal: 8,
 
-    color: '#2d6628',
+      color: '#2d6628',
 
-    fontSize: 14,
-    marginBottom: 8,
-  },
-  entrada: {
-    flex: 1,
-    borderWidth: 1,
-    borderColor: erro ? '#FF0000' : '#4A4A4A',
-    color: erro ? '#FF0000' : '#4A4A4A',
-    borderRadius: 16,
-    height: 53,
+      fontSize: 14,
+      marginBottom: 8,
+    },
+    entrada: {
+      flex: 1,
+      borderWidth: 1,
+      borderColor: erro ? '#FF0000' : '#4A4A4A',
+      color: erro ? '#FF0000' : '#4A4A4A',
+      borderRadius: 16,
+      height: 53,
 
-    padding: 16,
-    margin: 8,
+      padding: 16,
+      margin: 8,
 
-    textAlign: 'center',
-    fontSize: 16,
-  },
-  botao: {
-    margin: 8,
-    padding: 16,
-    backgroundColor: cor,
-    borderRadius: 16,
-  },
-});
+      textAlign: 'center',
+      fontSize: 16,
+    },
+    botao: {
+      margin: 8,
+      padding: 16,
+      backgroundColor: cor,
+      borderRadius: 16,
+    },
+  });
